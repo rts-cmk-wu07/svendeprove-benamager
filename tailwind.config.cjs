749ef3e972825bdab314e11b6b1882301c6f489f
benamager <benaprivat@gmail.com>
2023-03-14 09:38:26 +0100

    Setup colors, metadata & fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.jsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: "##EAEAEA",
+        primary: "#5E2E53",
+        secondary: "#E1A1E9",
+        black: "#000000"
+      }
+    },
   },
   plugins: [],
 }
