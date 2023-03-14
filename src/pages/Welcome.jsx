cc98637ebb1e705036c6b8d0f29a2f5fa7b7accf
benamager <benaprivat@gmail.com>
2023-03-14 12:41:45 +0100

    Add welcome page and tailwind config

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,17 @@
+import splashImage from "../assets/images/splash-image.jpg"
+import { Link } from "react-router-dom";
+
 export default function Welcome() {
   return (
-    <div>welcome</div>
+    <div className="grid h-[100dvh]">
+      <img src={splashImage} alt="Image of guy dancing made with splash paint." className="grid-area-1 h-full object-cover" />
+      <h1 className="grid-area-1 leading-[1.25rem] self-center translate-y-11">
+        <span className="ml-11 font-roboto text-[36px] text-stroke-top uppercase tracking-tighter block">Landrup</span>
+        <br />
+        <span className="ml-11 font-racingSansOne text-[4.5rem] text-stroke-bottom">Dans</span>
+        <hr className="border-none bg-primary h-4 w-[60%] mt-4" />
+      </h1>
+      <Link to="activities" className="grid-area-1 bg-primary self-end w-[70%] mx-auto text-white p-4 text-center mb-[15%] rounded-2xl text-base">Kom i gang</Link>
+    </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,36 @@ module.exports = {
         primary: "#5E2E53",
         secondary: "#E1A1E9",
         black: "#000000"
-      }
+      },
+      fontFamily: {
+        "roboto": ["Roboto", "sans-serif"],
+        "racingSansOne": ["Racing Sans One", "cursive"],
+      },
+      fontSize: {
+        base: "1.125rem",
+        lg: "1.5rem",
+        xl: "2.25rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    // ...
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".grid-area-1": {
+          gridArea: "1 / 1 / 1 / 1",
+        },
+        ".text-stroke-top": {
+          WebkitTextFillColor: "transparent",
+          WebkitTextStroke: "2px #431567",
+        },
+        ".text-stroke-bottom": {
+          WebkitTextFillColor: "#E856EB",
+          WebkitTextStroke: "1px #000000",
+        },
+      }
+
+      addUtilities(newUtilities)
+    }
+  ],
 }
