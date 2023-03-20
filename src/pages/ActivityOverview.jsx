bedea7ac54ca28cf08874cb0fbab84e963b95b99
benamager <benaprivat@gmail.com>
2023-03-20 13:43:49 +0100

    Add error handling to activity overview

--- src/pages/ActivityOverview.jsx
+++ src/pages/ActivityOverview.jsx
@@ -5,6 +5,7 @@ import useAxios from "../hooks/useAxios";
 import { useParams } from "react-router-dom";
 import InfoCard from "../components/InfoCard";
 import { FiCalendar } from "react-icons/fi";
+import { BiErrorAlt } from "react-icons/bi"
 
 export default function ActivityOverview() {
   const { activityId } = useParams()
@@ -17,20 +18,34 @@ export default function ActivityOverview() {
 
   return (
     <div className="mx-5 mt-5 mb-[100px]">
-      <h1 className="text-xl text-white mb-5 text-ellipsis whitespace-nowrap overflow-hidden">{data?.name}</h1>
-      <ul>
-        {activityUsers.length === 0 &&
-          <InfoCard
-            icon={FiCalendar}
-            className="mt-[125px]"
-            title="Ingen elever i din klasse"
-            description="Det var ærgeligt"
-            linkText="Se kalender"
-            linkTo="/calendar"
-          />
-        }
-        {activityUsers.map(user => <li className="text-base text-white">{user.username}</li>)}
-      </ul>
+      {error &&
+        <InfoCard
+          icon={BiErrorAlt}
+          className="mt-[200px] mx-3"
+          title="Ikke fundet"
+          description="Aktiviteten du leder efter blev ikke fundet..."
+          linkText="Gå tilbage"
+          linkTo="/activities"
+        />
+      }
+      {data &&
+        <>
+          <h1 className="text-xl text-white mb-5 text-ellipsis whitespace-nowrap overflow-hidden">{data?.name}</h1>
+          <ul>
+            {activityUsers.length === 0 &&
+              <InfoCard
+                icon={FiCalendar}
+                className="mt-[125px]"
+                title="Ingen elever i din klasse"
+                description="Det var ærgeligt"
+                linkText="Se kalender"
+                linkTo="/calendar"
+              />
+            }
+            {activityUsers.map(user => <li className="text-base text-white">{user.username}</li>)}
+          </ul>
+        </>
+      }
     </div>
   );
 }
